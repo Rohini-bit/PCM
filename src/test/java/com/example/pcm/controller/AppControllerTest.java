fcf979b0f949ab8903f0ca4b7ece6e7fc9c57428
Rohini-bit <rohini.merikenapalli@gmail.com>
2022-10-18 09:38:10 -0500

    test cases added

---
+++ src/test/java/com/example/pcm/controller/AppControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.pcm.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppControllerTest {
+
+	@Test
+	void test() {
+		System.out.println("testpass");
+	}
+
+}
